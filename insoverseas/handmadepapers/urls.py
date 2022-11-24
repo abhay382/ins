13b7270798152988af2eef377d22e3414d98cf66
Abhay Singh <62319333+abhay382@users.noreply.github.com>
2022-11-24 08:07:19 -0800

    Update urls.py

--- insoverseas/handmadepapers/urls.py
+++ insoverseas/handmadepapers/urls.py
@@ -3,9 +3,24 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-     path('', views.index, name='index'),
+    
 
-
+  path('', views.index, name='index'),
+     path('gallery/', views.Gallery, name='index'),
+     path('about/', views.about, name='index'),
+     path('contact/', views.contact, name='index'),
+     path('factoury/', views.fatory, name='index'),
+     path('gallery/', views.Gallery, name='index'),
+     path('seedpapers/', views.seed, name='index'),
+     path('leatherjournal/', views.Leatherjournal, name='index'),
+     path('christmas/', views.christ, name='index'),
+     path('paperbags/', views.bag, name='index'),
+     path('handmadepaper/', views.handmade, name='index'),
+     path('lamp/', views.lamp, name='index'),
+     path('boxes/', views.boxes, name='index'),
+     path('gift/', views.gift, name='index'),
+     path('cards/', views.cards, name='index'),
+     path('query/', views.query, name='index'),
 
 
 ]
