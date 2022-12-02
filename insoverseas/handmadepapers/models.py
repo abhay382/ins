56250bc5941a2e67dcd221b4984c15b6df4aeba9
Abhay Singh <62319333+abhay382@users.noreply.github.com>
2022-12-02 04:53:04 -0800

    Update models.py

--- insoverseas/handmadepapers/models.py
+++ insoverseas/handmadepapers/models.py
@@ -10,3 +10,15 @@ class Contact(models.Model):
 
     def __str__(self):
         return self.name
+    
+    
+    
+    
+    class Query(models.Model):
+    msg_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=90)
+    email = models.CharField(max_length=70, default="")
+    desc = models.CharField(max_length=500, default="")
+
+    def __str__(self):
+        return self.name
